86184984ba6966153b1ba208199dec1e82b4848f
Charlie W <cw5790@gmail.com>
2024-03-31 00:06:52 +0000

    Add fonts

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,16 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import stylesheet from "~/styles/tailwind.css?url";
+import fontStylesheet from "~/styles/fonts.css?url";
+import { Header } from "~/components/header/header";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+  { rel: "stylesheet", href: fontStylesheet },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -16,7 +26,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Header />
+        <main className="flex-1">
         {children}
+
+        </main>
         <ScrollRestoration />
         <Scripts />
       </body>

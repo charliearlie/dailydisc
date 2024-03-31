92f066c099db546df3a8b03c27d8dcbe3deffc4e
Charlie W <cw5790@gmail.com>
2024-03-31 00:06:26 +0000

    Vite and TS config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dotenvExpand from "dotenv-expand";
 
 installGlobals();
 
-export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  if (mode === "development") {
+    const env = loadEnv(mode, process.cwd(), "");
+    dotenvExpand.expand({ parsed: env });
+  }
+
+  return {
+    plugins: [remix(), tsconfigPaths()],
+  };
 });

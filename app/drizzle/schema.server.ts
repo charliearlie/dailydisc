d72f3560b043b3e802a3a73e3cbec3a17e49d3db
Charlie W <cw5790@gmail.com>
2024-04-01 00:42:28 +0100

    Add reviews to schema

--- app/drizzle/schema.server.ts
+++ app/drizzle/schema.server.ts
@@ -6,6 +6,9 @@ import {
   primaryKey,
 } from "drizzle-orm/sqlite-core";
 
+/**
+ * User definition and relationships
+ */
 export const users = sqliteTable("users", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
   username: text("username").notNull().unique(),
@@ -19,6 +22,9 @@ export const users = sqliteTable("users", {
     .default(sql`(unixepoch())`),
 });
 
+/**
+ * Album definition and relationships
+ */
 export const albums = sqliteTable("albums", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
   title: text("title").notNull(),
@@ -31,6 +37,15 @@ export const albums = sqliteTable("albums", {
     .notNull()
     .default(sql`(unixepoch())`),
 });
+
+export const albumsRelations = relations(albums, ({ many }) => ({
+  artistsToAlbums: many(artistsToAlbums),
+  reviews: many(reviews),
+}));
+
+/**
+ * Artist definition and relationships
+ */
 
 export const artists = sqliteTable("artists", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
@@ -57,12 +72,8 @@ export const artistsToAlbums = sqliteTable(
   },
   (t) => ({
     pk: primaryKey({ columns: [t.artistId, t.albumId] }),
-  })
+  }),
 );
-
-export const albumsRelations = relations(albums, ({ many }) => ({
-  artistsToAlbums: many(artistsToAlbums),
-}));
 
 export const artistsRelations = relations(artists, ({ many }) => ({
   artistsToAlbums: many(artistsToAlbums),
@@ -79,5 +90,38 @@ export const artistsToAlbumsRelations = relations(
       fields: [artistsToAlbums.albumId],
       references: [albums.id],
     }),
-  })
+  }),
 );
+
+/**
+ * Review definition and relationships
+ */
+export const reviews = sqliteTable("reviews", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  albumId: integer("album_id", { mode: "number" })
+    .notNull()
+    .references(() => albums.id),
+  userId: integer("user_id", { mode: "number" })
+    .notNull()
+    .references(() => users.id),
+  rating: integer("rating").notNull(),
+  review: text("review"),
+  favouriteTrack: text("favourite_track"),
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+  updatedAt: integer("updated_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+});
+
+export const reviewsRelations = relations(reviews, ({ one }) => ({
+  album: one(albums, {
+    fields: [reviews.albumId],
+    references: [albums.id],
+  }),
+  user: one(users, {
+    fields: [reviews.userId],
+    references: [users.id],
+  }),
+}));

b25120d30d6f09fc8025cc22d4e4d73006920b1c
Charlie W <cw5790@gmail.com>
2024-04-26 23:56:57 +0100

    Improve review design

--- app/components/reviews/review-card.tsx
+++ app/components/reviews/review-card.tsx
@@ -9,22 +9,21 @@ type Props = {
 };
 export const ReviewCard = ({ review }: Props) => {
   return (
-    <Card>
-      <CardHeader>
-        <div className="flex w-full items-center justify-between">
-          <h4>{review.user.username}</h4>
-          <span className="text-sm">
-            {formatDistance(new Date(review.createdAt), new Date(), {
-              addSuffix: true,
-            })}
-          </span>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <p className="text-lg">Rating: {review.rating}</p>
-        <p className="text-lg">Favourite Track: {review.favouriteTrack}</p>
-        {review.review && <p>Review: {review.review}</p>}
-      </CardContent>
-    </Card>
+    <div className="w-full">
+      <div className="flex items-center justify-between">
+        <h4 className="text-lg font-bold">{review.user.username}</h4>
+        <span className="text-xs">
+          {formatDistance(new Date(review.createdAt), new Date(), {
+            addSuffix: true,
+          })}
+        </span>
+      </div>
+
+      <p>Favourite Track: {review.favouriteTrack}</p>
+      {review.review && <p>Review: {review.review}</p>}
+      <p className="my-2">
+        <span className="text-xl font-semibold">{review.rating / 2} / </span>10
+      </p>
+    </div>
   );
 };

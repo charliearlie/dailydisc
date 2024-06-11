d42833a1cd04553e4ac3658089a5b290648066dc
Charlie W <cw5790@gmail.com>
2024-06-11 23:23:18 +0100

    Getting closer with Edge functions

--- app/util/utils.ts
+++ app/util/utils.ts
@@ -80,5 +80,30 @@ export function parseVercelId(id: string | null) {
   }
   const proxyRegion = parts[0];
   const computeRegion = parts[parts.length - 2];
-  return { proxyRegion, computeRegion };
+  return {
+    proxyRegion: regions[proxyRegion],
+    computeRegion: regions[computeRegion],
+  };
 }
+
+export const regions: Record<string, string> = {
+  sfo1: "San Francisco",
+  iad1: "Washington, D.C.",
+  pdx1: "Portland",
+  cle1: "Cleveland",
+  gru1: "São Paulo",
+  hkg1: "Hong Kong",
+  hnd1: "Tokyo",
+  icn1: "Seoul",
+  kix1: "Osaka",
+  sin1: "Singapore",
+  bom1: "Mumbai",
+  syd1: "Sydney",
+  cdg1: "Paris",
+  arn1: "Stockholm",
+  dub1: "Dublin",
+  lhr1: "London",
+  fra1: "Frankfurt",
+  cpt1: "Cape Town",
+  dev1: "localhost",
+};

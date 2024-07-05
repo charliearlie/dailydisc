da2715cfde264ac8656e727b8bafaee745ceef3c
Charlie W <cw5790@gmail.com>
2024-07-06 00:59:23 +0100

    Test sending random album to Discord

---
+++ app/routes/api.pick-album-without-mutation/route.tsx
@@ -0,0 +1,47 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { and, eq, sql } from "drizzle-orm";
+import { db } from "~/drizzle/db.server";
+import { albums } from "~/drizzle/schema.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const apiKey = request.headers.get("x-api-key");
+  const serverApiKey = process.env.API_KEY;
+
+  if (!apiKey || apiKey !== serverApiKey) {
+    return json(
+      {
+        error:
+          "You are not authorised to hit this endpoint, please provide a valid API key",
+      },
+      { status: 403 },
+    );
+  }
+
+  console.log("Picking today's album");
+
+  const todaysDate = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "Europe/London" }),
+  );
+  todaysDate.setUTCHours(0, 0, 0, 0);
+
+  const scheduledAlbum = await db.query.albums.findFirst({
+    where: and(eq(albums.listenDate, todaysDate), eq(albums.archived, 0)),
+  });
+
+  if (scheduledAlbum) {
+    console.log("Todays album is ", scheduledAlbum.title);
+
+    return json({ randomAlbum: scheduledAlbum });
+  } else {
+    const [randomAlbum] = await db
+      .select()
+      .from(albums)
+      .where(eq(albums.archived, 0))
+      .orderBy(sql.raw("RANDOM()"))
+      .limit(1);
+
+    console.log("Todays album is ", randomAlbum.title);
+
+    return json({ randomAlbum });
+  }
+};

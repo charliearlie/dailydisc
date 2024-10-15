9f3da1ce2f2f41af71b7c8943ed2891908907920
Charlie W <cw5790@gmail.com>
2024-10-15 18:30:14 +0100

    Recalculate average rating on review deletion

--- app/routes/resource.delete/route.tsx
+++ app/routes/resource.delete/route.tsx
@@ -1,7 +1,8 @@
 import { ActionFunctionArgs, json } from "@remix-run/node";
 import { eq } from "drizzle-orm";
 import { db } from "~/drizzle/db.server";
-import { reviews } from "~/drizzle/schema.server";
+import { albums, reviews } from "~/drizzle/schema.server";
+import { getAverageRatingAndReviewCount } from "~/services/album.server";
 import { getUserId } from "~/services/session";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
@@ -12,7 +13,40 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   const userId = formData.get("userId");
 
   if (userId === `${loggedInUserId}`) {
-    await db.delete(reviews).where(eq(reviews.id, Number(reviewId)));
+    const reviewToDelete = await db.query.reviews.findFirst({
+      where: eq(reviews.id, Number(reviewId)),
+      columns: {
+        albumId: true,
+        id: true,
+        rating: true,
+      },
+    });
+
+    if (!reviewToDelete) {
+      return json({
+        status: "error" as const,
+      });
+    }
+
+    const { averageRating, reviewCount } = await getAverageRatingAndReviewCount(
+      reviewToDelete.albumId,
+    );
+
+    const newAverageRating = Number(
+      (
+        (averageRating * reviewCount - reviewToDelete.rating) /
+        (reviewCount - 1)
+      ).toFixed(1),
+    );
+
+    await db.delete(reviews).where(eq(reviews.id, reviewToDelete.id));
+
+    await db
+      .update(albums)
+      .set({
+        averageRating: newAverageRating,
+      })
+      .where(eq(albums.id, reviewToDelete.albumId));
 
     return json({
       status: "success" as const,

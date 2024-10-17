c06147d45bf9f690822ca79c38a8d30331b94c53
Charlie W <cw5790@gmail.com>
2024-10-17 20:25:50 +0100

    Remove limit and offset

--- app/routes/api.artists.tsx
+++ app/routes/api.artists.tsx
@@ -1,18 +1,12 @@
-import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { db } from "~/drizzle/db.server";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const url = new URL(request.url);
-  const limit = Number(url.searchParams.get("limit") || "50");
-  const offset = Number(url.searchParams.get("offset") || "0");
-
+export const loader = async () => {
   const artists = await db.query.artists.findMany({
     columns: {
       id: true,
       name: true,
     },
-    limit,
-    offset,
   });
 
   return json({ artists });
